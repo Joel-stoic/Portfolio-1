b7cd7386977c9175359044641f8dbabd80613f38
Joel-stoic <joelimmanual.off@gmail.com>
2025-05-18 10:45:56 -0700

    Edited the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,8 +12,6 @@ export const Navbar = () => {
             <motion.div initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}}className='social'>
                 <a href="#"><img src="./facebook.png" alt="" /></a>
                 <a href="#"><img src="./instagram.png" alt="" /></a>
-                <a href="#"><img src="./youtube.png" alt="" /></a>
-                <a href="#"><img src="./public/dribbble.png" alt="" /></a>
             </motion.div>
         </div>
     </div>
